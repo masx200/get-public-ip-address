7c07589afe880abf1d48d176e9779123d873e8d1
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-12 15:30:38 +0800

    Update check_response_ok.ts

--- check_response_ok.ts
+++ check_response_ok.ts
@@ -1,10 +1,10 @@
 export async function check_response_ok(response: Response) {
-    const { status, url } = response;
+    const { status, url ,headers} = response;
     if (!response.ok) {
         const body = await response.text();
         throw Error(
             "response failure ok:" +
-                JSON.stringify({ status, url }) +
+                JSON.stringify({ status, url,headers:Object.fromEntries(headers) }) +
                 "\n" +
                 body,
         );
