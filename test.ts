7ff7ba4644664b4118f872045cc782efea8db579
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-12 15:16:22 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,19 +3,16 @@ import { getPublicIpv4 } from "./getPublicIpv4.ts";
 import { getPublicIpv6 } from "./getPublicIpv6.ts";
 
 Deno.test("getPublicIpv6", async () => {
-      try {
-          console.log(await getPublicIpv6());
+    try {
+        console.log(await getPublicIpv6());
     } catch (error) {
         console.warn("error", error);
         console.warn("message", String(error));
     }
-  
 });
 Deno.test("getPublicIpv4", async () => {
     console.log(await getPublicIpv4());
 });
 Deno.test("getPublicIpAddress", async () => {
-   
-        console.log(await getPublicIpAddress());
- 
+    console.log(await getPublicIpAddress());
 });
